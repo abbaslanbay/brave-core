e5ba3050c0d740efbd0934c1b8003c0d4cda7b6e
Francois Marier <francois@brave.com>
2023-04-19 16:26:38 -0700

    Disable site affiliation fetcher (fixes brave/brave-browser#29872)

---
+++ chromium_src/components/password_manager/core/browser/affiliation/hash_affiliation_fetcher.cc
@@ -0,0 +1,8 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_HASH_AFFILIATION_FETCHER_START_REQUEST return;
+#include "src/components/password_manager/core/browser/affiliation/hash_affiliation_fetcher.cc"
+#undef BRAVE_HASH_AFFILIATION_FETCHER_START_REQUEST
